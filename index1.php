f18011c30cda9793a85f3ae9bdd4835e9b665203
Difelice-e <difelice.emanuele@gmail.com>
2022-03-29 14:12:21 +0200

    creazione file

---
+++ index1.php
@@ -0,0 +1,22 @@
+<?php
+    $partite = [
+        [
+            'casa' => 'Basket Milano',
+            'opsite' => 'Basket Roma',
+            'punti_casa' => 45,
+            'punti_opsite' => 21,
+        ],
+        [
+            'casa' => 'Basket Pescara',
+            'opsite' => 'Basket Chieti',
+            'punti_casa' => 65,
+            'punti_opsite' => 81,
+        ],
+        [
+            'casa' => 'Basket Cosenza',
+            'opsite' => 'Basket Vicenza',
+            'punti_casa' => 27,
+            'punti_opsite' => 69,
+        ],
+    ]
+?>
